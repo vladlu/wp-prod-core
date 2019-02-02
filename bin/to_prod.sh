9139cd9ac34552021cbb32c91e4512c4aea70195
Vladislav Luzan <hey@vlad.lu>
2019-02-02 14:42:08 +0900

    Some minor improvements

--- bin/to_prod.sh
+++ bin/to_prod.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-cd "$SCRIPTPATH/.."
+devSystem_ROOT="$SCRIPTPATH/.."
+project_ROOT="$devSystem_ROOT/../.."
+
+rules_dir="$devSystem_ROOT/.rules.d"
 
 lock="locks/prod"
 unlock="locks/dev"
 
-rules_dir='.rules.d'
+
+cd $devSystem_ROOT
+
 
 if [ -d "webpack/node_modules" ]; then
     if [ ! -f "$lock" ]; then
         touch "$lock"
         rm -f "$unlock"
 
-        ../utilities/rules_parser "$rules_dir"
+        utilities/rules_parser "$rules_dir"
 
 
 
@@ -22,7 +27,7 @@ if [ -d "webpack/node_modules" ]; then
         if [ -f "$rules_dir/mkdir.tsv" ]; then
             while IFS='' read -r filepath
             do
-                mkdir -p "../$(dirname "$filepath")"
+                mkdir -p "$project_ROOT/$(dirname "$filepath")"
             done < "$rules_dir/mkdir.tsv"
         fi
 
@@ -33,7 +38,7 @@ if [ -d "webpack/node_modules" ]; then
             while IFS=$'\t' read -r from to
             do
                 if [[ "$from" != "dev/"* ]] && [[ "$to" != "dev/"* ]]; then
-                    mv "../$to" "../$from"
+                    mv "$project_ROOT/$to" "$project_ROOT/$from"
                 fi
             done < "$rules_dir/maybe_rename.tsv"
         fi
@@ -43,7 +48,7 @@ if [ -d "webpack/node_modules" ]; then
         if [ -f "$rules_dir/substitute.tsv" ]; then
             while IFS=$'\t' read -r dev prod file
             do
-                sed -i '' -e "s/$dev/$prod/g" ../"$file"
+                sed -i '' -e "s/$dev/$prod/g" "$project_ROOT/$file"
             done < "$rules_dir/substitute.tsv"
         fi
 
@@ -51,7 +56,7 @@ if [ -d "webpack/node_modules" ]; then
         if [ -f "$rules_dir/new_dir.tsv" ]; then
             while IFS=$'\t' read -r dir
             do
-                mkdir -p "../$dir"
+                mkdir -p "$project_ROOT/$dir"
             done < "$rules_dir/new_dir.tsv"
         fi
 
@@ -59,7 +64,7 @@ if [ -d "webpack/node_modules" ]; then
         if [ -f "$rules_dir/copy.tsv" ]; then
             while IFS=$'\t' read -r from to
             do
-                cp "../$from" "../$to"
+                cp "$project_ROOT/$from" "$project_ROOT/$to"
             done < "$rules_dir/copy.tsv"
         fi
 
@@ -77,7 +82,7 @@ if [ -d "webpack/node_modules" ]; then
             while IFS=$'\t' read -r to from
             do
                 if [[ $to == *".css" ]]; then
-                    mv "../$to.css" "../$to"
+                    mv "$project_ROOT/$to.css" "$project_ROOT/$to"
                 fi
             done < "$rules_dir/webpack.tsv"
         fi
